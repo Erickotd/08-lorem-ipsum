df92b0cf0a296a33c9c5ca729b04ec4b5f754ae2
erickotd <erickotd@gmail.com>
2020-11-03 11:44:26 +0100

    finish project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    clearText();
+    let amount = parseInt(count);
+    if (amount <= 0) amount = 1;
+    if (amount > 8) amount = 8;
+    let lorem = data.slice(0, amount);
+    setText(lorem);
+  };
+
+  const clearText = () => {
+    setText([]);
+  };
+
+  const handlerChange = (e) => {
+    setCount(e.target.value);
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handlerSubmit}>
+        <label htmlFor="amount">Paragraphs</label>
+        <input
+          id="amount"
+          type="number"
+          name="amount"
+          value={count}
+          onChange={handlerChange}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      {text.map((paragraph, index) => {
+        return <p>{paragraph}</p>;
+      })}
+    </section>
+  );
 }
 
 export default App;
